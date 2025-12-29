2a73f068848091c0e5d9c544ef37662823cb231f
Dilip9 <dilip.verma940805@gmail.com>
2025-12-29 22:51:43 +0530

    Initial commit for ratelimiter

---
+++ src/main/java/gouri/notification/notification_rate_limitor/NotificationRateLimitorApplication.java
@@ -0,0 +1,13 @@
+package gouri.notification.notification_rate_limitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationRateLimitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationRateLimitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gouri/notification/notification_rate_limitor/NotificationRateLimitorApplicationTests.java
@@ -0,0 +1,13 @@
+package gouri.notification.notification_rate_limitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationRateLimitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
